581d69333ca566da2074eb4d6f76f4b1d60a4bcd
Mark Santolucito <mark.santolucito@yale.edu>
2021-11-21 11:00:05 -0500

    basic rendering mostly working

--- src/tools/parsedot/Main.hs
+++ src/tools/parsedot/Main.hs
@@ -1,7 +1,7 @@
 ----------------------------------------------------------------------------
 -- |
 -- Module      :  Main
--- Maintainer  :  Morgan Zee, Wonhyuk Choi, Mark Santolucito
+-- Maintainer  :  Morgan Zee, Wonhyuk Choi
 -- 
 -- Description : Parse a dot file with labels and transitions from TSL and re-render the lables in a human readable form
 -----------------------------------------------------------------------------
@@ -24,11 +24,8 @@ import System.Environment
 
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Monoid
 import qualified Data.Text.IO as T
-
-
---TODO: 
---remove logical operators, decode predicates and updates, put the AND and NOT back to decode 
 
 andToken = "&#8743;"
 notToken = "&#172;"
@@ -37,58 +34,49 @@ endToken = "&#10;"
 main :: IO ()
 main = do
        args <- getArgs
-       contents <- T.readFile $ head args
+       let filename = head args
+       contents <- T.readFile filename
 
-       --translateLabeltooltip
+       let newContent = T.unlines $ map translateLabeltooltip $ T.lines contents
 
-       --Splitting at &#10 which indicates the end of a label 
-       let terms =  getTerms contents
-       let terms' = map decoder terms
+       T.writeFile ("parsed_" ++ filename) newContent
 
-       mapM_ print terms'
+translateLabeltooltip :: Text -> Text
+translateLabeltooltip lineOfDotFile =
+  let
+    transitionInfo = fst $ T.breakOn "[label=" lineOfDotFile
+    dropStart = T.drop (T.length "labeltooltip=\"") 
+    dropEnding = T.dropEnd 3 --drop last three chars ( "]; ) of tooltip line
+    tooltip = dropStart $ dropEnding $ snd $ T.breakOn "labeltooltip=\"" lineOfDotFile
+    terms = getTerms $ T.strip $ tooltip
+    translatedTerms = map decodeWrapper terms
+  in 
+    if T.isInfixOf "labeltooltip" lineOfDotFile
+    then transitionInfo <> "[label=\"" <> (combinepsus translatedTerms) <> "\\l\"];"
+    else lineOfDotFile
+
+combinepsus :: [([Text], [Text])] -> Text
+combinepsus = T.intercalate "\\l\\l" . map (\(ps,us) -> (T.intercalate " /\\ " ps) <> "\\l===\n" <> (T.intercalate "\\l" us)) 
 
 getTerms :: Text -> [([Text], [Text])]
 getTerms t = let
-  lines = T.splitOn endToken t
+  lines = init $ T.splitOn endToken t
   predsUpdates = map ((\[x1,x2] -> (x1,x2)). T.splitOn "/") lines 
   splitAnds = (\f (x,y) -> (f x, f y)) (T.splitOn andToken)
  in
   map splitAnds predsUpdates
 
 
-decoder :: ([Text], [Text]) -> _
-decoder (ps, us) = let
+decodeWrapper :: ([Text], [Text]) -> ([Text],[Text])
+decodeWrapper (ps, us) = let
   unpackClean = 
     T.unpack. 
-    T.replace notToken "". --TODO keep track of negations to combine with... something
     T.replace "+" "" --TODO figure out why we have "+" in the dot file output
-  tryInput  = map (decodeInputAP. unpackClean) ps
-  tryOutput = map (decodeOutputAP. unpackClean) us
-  generatePredString   = either (const "ERR") (tslFormula id. Check)
-  generateUpdateString = either (const "ERR") (tslFormula id. (uncurry Update))
+  preprocess decode = map (decode. unpackClean) . filter (not. T.isPrefixOf notToken) 
+  tryInput  = preprocess decodeInputAP ps
+  tryOutput = preprocess decodeOutputAP us
+  generatePredString   = either (const "ERR") (T.pack. tslFormula id. Check)
+  generateUpdateString = either (const "ERR") (T.pack. tslFormula id. (uncurry Update))
  in
   (map generatePredString tryInput, map generateUpdateString tryOutput)
-
-{-
-safeDecode :: (String,String) -> _ 
-safeDecode c = 
-  let
-    parsedInput = decodeInputAP $ takeWhile (isAlphaNum)(fst c) 
-    parsedOutput = decodeOutputAP (snd c)
-  in 
-    (parsedInput, parsedOutput)
-
-decodeInputWrapper :: String -> (String, String)
-decodeInputWrapper splitDash = 
-  case splitDash of 
-    "&#8743;" -> "AND" 
-    "&#172;" -> "NOT"
-    other -> 
-      case decodeOutputAP other of 
-        Left err -> "err" 
-        Right result -> result       
--}
-
---Helpful: 
--- | https://stackoverflow.com/questions/7867723/haskell-file-reading
 
