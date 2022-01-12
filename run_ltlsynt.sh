d7bc0a75753621bb6946283368e146f9bdaa69b7
Mark Santolucito <mark.santolucito@yale.edu>
2022-01-12 11:14:36 -0500

    fix up some loose ends

--- run_ltlsynt.sh
+++ run_ltlsynt.sh
@@ -5,8 +5,8 @@ LTL=$(syfco $1 -f ltlxba -m fully)
 IN=$(syfco $1 --print-input-signals)
 OUT=$(syfco $1 --print-output-signals)
 ltlsynt --formula="$LTL" --ins="$IN" --outs="$OUT" > $filename.hoa
-./parsedot $filename.hoa
-sed -i 1d parsed_$filename.hoa
+sed -i 1d $filename.hoa
+./parsehoa $filename.hoa > parsed_$filename.hoa
 autfilt parsed_$filename.hoa --deterministic --dot > $filename.dot
 sed -i 's/|/|\\n/g' $filename.dot
 dot -Tpng $filename.dot -o $filename.png
--- run_with_strix.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-strix="../Strix/scripts/strix_tlsf.sh"
-cat > file.tlsf
-$strix file.tlsf > temp.tmp
-head -n 1 temp.tmp
