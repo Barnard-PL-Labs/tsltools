1eea0ee14e0e66c654885a76a4a38e3f1b36304f
wfishell <wf2322@columbia.edu>
2025-05-23 04:42:54 -0400

    Adding a max consistency checks, preventing a 2^n check on the predic… (#77)
    * Adding a max consistency checks, preventing a 2^n check on the predicates for smts which caused very slow or virtualy unsynthesizable specs, despite their simplicity
    
    * requested changes for consistency checking

--- src/TSL/ModuloTheories/ConsistencyChecking.hs
+++ src/TSL/ModuloTheories/ConsistencyChecking.hs
@@ -2,42 +2,65 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TupleSections #-}
 
--- |
 -- Module      :  TSL.ModuloTheories.ConsistencyChecking
--- Description :  Adds partial semantics back to uninterpreted functions by checking which combinations of uninterpreted predicates are satisfiable. For each unsatisfiable combination, the negation of that combination is added as an assumption.
+-- Description :  Adds partial semantics back to uninterpreted functions by checking which combinations
+--                of uninterpreted predicates are satisfiable.  To avoid an exponential blow‑up in SMT
+--                calls, we now cap the number of combinations examined.
 -- Maintainer  :  Wonhyuk Choi
 module TSL.ModuloTheories.ConsistencyChecking
   ( generateConsistencyAssumptions,
     consistencyDebug,
     ConsistencyDebugInfo (..),
   )
-where
+  where
 
-import Control.Monad.Trans.Except
-import TSL.Base.Ast
-  ( AstInfo (..),
-    SymbolInfo (..),
-    deduplicate,
-  )
-import TSL.Error (Error, errConsistency)
-import TSL.ModuloTheories.Debug (IntermediateResults (..))
-import TSL.ModuloTheories.Predicates
-  ( TheoryPredicate (..),
-    enumeratePreds,
-    pred2Smt,
-    pred2Tsl,
-    predInfo,
-    predTheory,
-  )
-import TSL.ModuloTheories.Solver (solveSat)
-import TSL.ModuloTheories.Theories
-  ( Theory,
-    TheorySymbol,
-    isUninterpreted,
-    smtSortDecl,
-    symbol2Smt,
-    symbolType,
-  )
+import           Control.Monad.Trans.Except
+import qualified Data.List                      as L
+import           Debug.Trace                    (trace)
+import Data.Maybe (fromMaybe)
+
+import           TSL.Base.Ast                   (AstInfo (..), SymbolInfo (..), deduplicate)
+import           TSL.Error                      (Error, errConsistency)
+import           TSL.ModuloTheories.Debug       (IntermediateResults (..))
+import           TSL.ModuloTheories.Predicates  (TheoryPredicate (..), enumeratePreds
+                                               , pred2Smt, pred2Tsl, predInfo, predTheory)
+import           TSL.ModuloTheories.Solver      (solveSat)
+import           TSL.ModuloTheories.Theories    (Theory, TheorySymbol, isUninterpreted
+                                               , smtSortDecl, symbol2Smt, symbolType)
+
+-- |Hard cap on how many predicate combinations we send to the SMT solver.
+--   This prevents run‑away exponential behaviour when many uninterpreted
+--   predicates appear in the spec.  Adjust as needed or expose it as a CLI flag.
+-- this number can be changed based on the problem, but even for simple synthesis problems the 
+-- number of predicates being checked was always become 2^n. This simplifies while getting the correct answer
+config_MAX_CONSISTENCY_CHECKS :: Maybe Int
+config_MAX_CONSISTENCY_CHECKS = Just 100
+
+
+generateConsistencyAssumptions
+  :: FilePath
+  -> [TheoryPredicate]
+  -> [ExceptT Error IO String]
+generateConsistencyAssumptions path preds =
+  map (fmap fst . consistencyChecking path) limitedCombos
+  where
+    allCombos    = enumeratePreds preds
+    limitedCombos = case config_MAX_CONSISTENCY_CHECKS of
+      Just n  -> take n allCombos
+      Nothing -> allCombos
+
+consistencyDebug
+  :: FilePath
+  -> [TheoryPredicate]
+  -> [ExceptT Error IO ConsistencyDebugInfo]
+consistencyDebug path preds =
+  map (fmap snd . consistencyChecking path) limitedCombos
+  where
+    allCombos    = enumeratePreds preds
+    limitedCombos = case config_MAX_CONSISTENCY_CHECKS of
+      Just n  -> take n allCombos
+      Nothing -> allCombos
+
 
 pred2Assumption :: TheoryPredicate -> String
 pred2Assumption p = "G " ++ pred2Tsl (NotPLit p) ++ ";"
@@ -46,76 +69,57 @@ data ConsistencyDebugInfo = ConsistencyDebugInfo IntermediateResults String
 
 instance Show ConsistencyDebugInfo where
   show (ConsistencyDebugInfo results assumption) =
-    "ConsistencyDebugInfo {\n"
-      ++ "  results: "
-      ++ show results
-      ++ ",\n"
-      ++ "  assumption: "
-      ++ assumption
-      ++ "\n"
-      ++ "}"
+       "ConsistencyDebugInfo {"               ++
+       "\n  results: "  ++ show results ++
+       ",\n  assumption: " ++ assumption ++
+       "\n}"
 
-generateConsistencyAssumptions ::
-  FilePath ->
-  [TheoryPredicate] ->
-  [ExceptT Error IO String]
-generateConsistencyAssumptions path preds =
-  map (fmap fst . consistencyChecking path) (enumeratePreds preds)
 
-consistencyDebug ::
-  FilePath ->
-  [TheoryPredicate] ->
-  [ExceptT Error IO ConsistencyDebugInfo]
-consistencyDebug path preds =
-  map (fmap snd . consistencyChecking path) (enumeratePreds preds)
-
-consistencyChecking ::
-  FilePath ->
-  TheoryPredicate ->
-  ExceptT Error IO (String, ConsistencyDebugInfo)
+consistencyChecking
+  :: FilePath
+  -> TheoryPredicate
+  -> ExceptT Error IO (String, ConsistencyDebugInfo)
 consistencyChecking solverPath pred = do
+  let query = pred2SmtQuery pred
   isSat <- solveSat solverPath query
   if isSat
-    then except $ errConsistency $ "Predicate " ++ show pred ++ " is satisfiable. No new assumption added."
-    else
-      let assumption = pred2Assumption pred
-          intermediateResults =
-            IntermediateResults (show pred) query (show isSat)
-          debugInfo =
-            ConsistencyDebugInfo intermediateResults assumption
-       in return (assumption, debugInfo)
-  where
-    query = pred2SmtQuery pred
+     then except $ errConsistency $ "Predicate " ++ show pred ++
+            " is satisfiable.  No new assumption added."
+     else do
+       let assumption          = pred2Assumption pred
+           intermediateResults = IntermediateResults (show pred) query (show isSat)
+           debugInfo           = ConsistencyDebugInfo intermediateResults assumption
+       trace ("[Consistency] adding assumption for " ++ show pred) $ return ()
+       return (assumption, debugInfo)
+
 
 pred2SmtQuery :: TheoryPredicate -> String
 pred2SmtQuery p = unlines [smtDeclarations, assertion, checkSat]
   where
     smtDeclarations = smtDecls (predTheory p) $ deduplicate $ predInfo p
-    assertion = "(assert " ++ pred2Smt p ++ ")"
-    checkSat = "(check-sat)"
+    assertion       = "(assert " ++ pred2Smt p ++ ")"
+    checkSat        = "(check-sat)"
 
 smtDecls :: Theory -> AstInfo TheorySymbol -> String
 smtDecls theory (AstInfo vars funcs preds) =
   unlines [logic, sortDecl, varDecls, funcDecls, predDecls]
   where
-    logic = "(set-logic " ++ show theory ++ ")"
-    sortDecl = smtSortDecl theory
-    varDecls = unlines $ map declConst vars
-    funcDecls = unlines $ map declFunc funcs
-    predDecls = unlines $ map declPred preds
+    logic      = "(set-logic " ++ show theory ++ ")"
+    sortDecl   = smtSortDecl theory
+    varDecls   = unlines $ map declConst vars
+    funcDecls  = unlines $ map declFunc funcs
+    predDecls  = unlines $ map declPred preds
+
     declConst (SymbolInfo x _) =
       "(declare-const " ++ symbol2Smt x ++ " " ++ symbolType x ++ ")"
-    declareFun retType (SymbolInfo f arity) =
-      if not (isUninterpreted f)
-        then ""
-        else
-          unwords
-            [ "(declare-fun",
-              symbol2Smt f,
-              "(",
-              unwords $ replicate arity $ show theory,
-              ")",
-              retType ++ ")"
-            ]
+
+    declareFun retType (SymbolInfo f arity)
+      | not (isUninterpreted f) = ""
+      | otherwise = unwords [ "(declare-fun"
+                            , symbol2Smt f
+                            , "("
+                            , unwords $ replicate arity $ show theory
+                            , ")"
+                            , retType ++ ")" ]
     declFunc = declareFun (show theory)
     declPred = declareFun "Bool"
